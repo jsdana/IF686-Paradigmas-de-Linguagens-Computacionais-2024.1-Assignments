98eeaba040a0c3b33d8c3c3bc3a48b750dbd4746
Juliana Silva <85325364+jsdana@users.noreply.github.com>
2024-04-22 10:02:07 -0300

    Update teste2.hs

--- teste2.hs
+++ teste2.hs
@@ -33,3 +33,47 @@ fatorial n  | (n == 0)      = 1
 
 addD :: Int -> Int -> Int
 addD a b = 2 * (a + b)
+
+
+vendas :: Int -> Int
+vendas 0 = 12
+vendas 1 = 14
+vendas 2 = 15
+vendas 3 = 20
+vendas x = 0
+
+total :: Int -> Int
+total x | (x == 0)  = vendas 0 
+        | otherwise = vendas x + total (x-1)
+
+--media :: Int -> Float
+--media x | (x == -1)  = total /   
+
+cabecalho :: String
+cabecalho = "Semana     Venda \n"
+
+printTotal :: Int -> String
+printTotal n = "Semana " ++ "   " ++ show(total n)
+
+media :: Int -> Int -> Float
+media s tot = fromIntegral (total s) / fromIntegral tot
+
+
+printTable :: Int -> IO()
+printTable n = putStr(cabecalho ++ printTotal n  ++ "\n")
+
+-----------------------------------------
+
+addEspacos :: Int -> String
+addEspacos 0 = ""
+addEspacos n = " " ++ addEspacos(n-1)
+
+paraDireita :: Int -> String -> String
+paraDireita n str = addEspacos n ++ str
+
+printWeek :: Int -> String
+printWeek n = paraDireita 2 (show n) ++ paraDireita 6 (show (vendas n)) ++ "\n"
+
+printWeeks :: Int -> String
+printWeeks 0 = printWeek 0
+printWeeks x = printWeek (x - 1) ++ printWeek x
